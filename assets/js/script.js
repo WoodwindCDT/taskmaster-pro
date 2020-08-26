a7251f4bbfa9cea8e232e7f5bc2bdfb24a84a5e5
Ender <woodwind.turbeville@gmail.com>
2020-08-26 15:57:53 -0500

    Added new UI Interfaces to allow drap & drop features as well as a remove drag and drop, also, I added UI ability for Mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,6 +172,71 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+    var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+    var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+    // To add task data to the temp array
+    tempArr.push({
+      text: text,
+      date: date
+     });
+    });
+    // To trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // To update array with new tasks object & save it
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+// Function for drag-to-remove card
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -183,5 +248,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
